b2d65cdcc09d6e9f567c85d48dcb5f733018ee27
(Robi) <(r.ruano3@gmail.com)>
2024-05-19 23:22:51 +0200

    ejercicio terminado

--- manipulacion_DOM.js
+++ manipulacion_DOM.js
@@ -1,16 +1,105 @@
-let listaBanner = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"];
-const elem = document.getElementsByClassName('imagen_portada');
-console.log(elem);
-elem.forEach(element){
-const img1 = document.createElement("img");
-img1.setAttribute("alt", "imagenes aleatorias");
-img1.setAttribute("src", alternarImagenes(listaBanner));
-element.appendChild(img1)
+/*
+Hacer una funcion que saque las fotos y un texto asociado a cada foto con sentido. Ej: 1.jpg con el texto: este es el jpg 1.
+*/
+//Variables
+let listaBanner = ["banner/1.jpg", "banner/2.jpg", "banner/3.jpg", "banner/4.jpg", "banner/5.jpg", "banner/6.jpg", "banner/7.jpg", "banner/8.jpg"];
+let textosBanner = ["texto0", "texto1", "texto2", "texto3", "texto4", "texto5", "texto6", "texto7"];
+let listaCiudades = ["Bilbao", "Teruel", "Sevilla", "Mostoles", "Azuqueca de Henares", "Valdelaparra de abajo"]
+let listaViajes = ["viajes/viajes-1.jpg", "viajes/viajes-2.jpg", "viajes/viajes-3.jpg", "viajes/viajes-4.jpg", "viajes/viajes-5.jpg", "viajes/viajes-6.jpg", "viajes/viajes-7.jpg"];
+let textosViajes = ["Gozadera en hamaca", "Camino al paraiso", "Mas perdido que el Pirri", "Viva Sevilla!", "Ole, ole y ole", "La curva del agujero", "Mi humilde castillo"]
+let parrafosLocos = ["mola mucho la playa, pero un verano en THE BRIDGE mola mas... (no llores, los adultos no lloran)",
+    "Las vistas molan pero el hospital mas cercano esta a 500km",
+     "los beneficios de tener google maps son incalculables",
+     "Lo que mas me gusta de sevilla son las mujeres sevillanas, pero la jiralda no esta mal",
+     "la verdad es que en esta plaza hace mas calor que en la comunión de Charmander", 
+     "esto es galicia fijo, todo humedo y con nubes, falta el de la gaita en la colina",
+     "Un humilde castillo de hace mil años rodeado de las madrigueras que usamos ahora de casas... si Carlos V levantara la cabeza..."];
+//Eventos
+
+//Funciones
+//1. crea una imagen y un texto aleatorios relacionados (no conseguido) a partir de dos listas
+function alternarImagenes(listaImagenes, listaTextos){
+    let img_text = [];
+    let imagenAleatoria;
+    let textoAleatorio;
+    let numeroAleatorio = Math.floor(Math.random()*listaImagenes.length);
+    imagenAleatoria = listaImagenes[numeroAleatorio];
+    indice = listaImagenes.indexOf(imagenAleatoria);
+    textoAleatorio = listaTextos[indice];
+    img_text[0] = imagenAleatoria;
+    img_text[1] = textoAleatorio;
+    return img_text;
 };
 
-function alternarImagenes(listaImagenes){
-    let imagenAleatoria;
-    let numeroAleatorio = Math.floor(Math.random*listaImagenes.length);
-    imagenAleatoria = listaImagenes[numeroAleatorio]
-    return imagenAleatoria
+//2. Funcion para crear una imagen con parametros un array y un texto.
+function crearImagen(jpgImagenes, textos){
+    let jpgImagen = alternarImagenes(jpgImagenes, textos)[0];
+    let texto = alternarImagenes(jpgImagenes, textos)[1];
+    const imagen = document.createElement("img");
+    imagen.setAttribute("src", jpgImagen);
+    imagen.setAttribute("alt", texto);
+    imagen.setAttribute("title", texto);
+    return imagen;
 };
+
+//3. Crear la imagen de la portada
+function imagenPortada(){
+    let imagenPortada = crearImagen(listaBanner, textosBanner);
+    let elemento1 = document.querySelector(".imagen_portada");
+    elemento1.appendChild(imagenPortada);
+};
+setTimeout(imagenPortada(), 500);//tengo que meter tiempo porque si no, lee antes el js que el DOM y da null.
+
+/*no se como manipular elementos de un Node.object de HTML. 
+let a = document.getElementsByClassName("imagen_portada");
+console.log(a);*/
+
+//4. crear las imagenes de los artículos y que no se repitan.
+function imagenArticulo(){
+    const articulos = document.querySelectorAll(".imagen_viaje");
+    let arrayComparacion = [];
+    articulos.forEach(function (e) {
+        let imagen;
+        do{
+        imagen = crearImagen(listaViajes, textosViajes);
+        arrayComparacion.push(imagen.src)
+        } while (arrayComparacion.indexOf(imagen.src) !== arrayComparacion.length-1)
+        e.appendChild(imagen);
+    }
+    )
+};
+imagenArticulo();
+
+//5. crear un titulo y un comentario dependiendo de la imagen que salga.
+function crearArticulos(){
+    const padre = document.querySelectorAll(".articulo");
+    padre.forEach(function(e){
+        const hermano = e.previousElementSibling;
+        const sobrino = hermano.firstElementChild;
+        const sobrino_imagen = sobrino.src;
+        let src = sobrino_imagen.substring(sobrino_imagen.length - 19);
+        const hijo1 = e.firstElementChild;
+        const hijo2 = e.lastElementChild;
+        let indice = listaViajes.indexOf(src);
+        hijo1.textContent = textosViajes[indice];
+        hijo2.textContent = parrafosLocos[indice];
+    })
+};
+crearArticulos()
+
+//6. crear una lista desplegable que según elijas construya un texto y un titulo en la seccion de las imagenes.
+function crearLista(){
+    const destinos = document.querySelector(".destinos");
+    const titulo = document.createElement("h2");
+    const lista = document.createElement("select")
+    lista.setAttribute("name", "listaDesplegable")
+    destinos.appendChild(titulo);
+    titulo.textContent = "Destinos";
+    destinos.appendChild(lista);
+    for (let i = 0; i < listaCiudades.length; i++){
+        let ciudad = document.createElement("option");
+        ciudad.textContent = listaCiudades[i];
+        lista.appendChild(ciudad);
+    }
+};
+crearLista();
